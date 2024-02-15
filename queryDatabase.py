e05ddc2fbc0f739fd58548be996898f7e46dea27
majidmoghadam <majid.moghadam@gmail.com>
2024-02-15 10:49:07 -0500

    initial upload of web pages

---
+++ queryDatabase.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+# Database setup
+def create_table():
+    conn = sqlite3.connect('receipts.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS receipts
+                 (id INTEGER PRIMARY KEY, date TEXT, time TEXT, liters REAL, total_dollar REAL, vehicle TEXT, odometer INTEGER, fuel_card TEXT, payment_method TEXT)''')
+    conn.commit()
+    conn.close()
+
+# Route for home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route for submitting the form
+@app.route('/submit', methods=['POST'])
+def submit():
+    date = request.form['date']
+    time = request.form['time']
+    liters = request.form['liters']
+    total_dollar = request.form['total_dollar']
+    vehicle = request.form['vehicle']
+    odometer = request.form['odometer']
+    fuel_card = request.form['fuel_card']
+    payment_method = request.form['payment_method']
+
+    conn = sqlite3.connect('receipts.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO receipts (date, time, liters, total_dollar, vehicle, odometer, fuel_card, payment_method) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+              (date, time, liters, total_dollar, vehicle, odometer, fuel_card, payment_method))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('home'))
+
+# Route for querying data
+@app.route('/query')
+def query():
+    conn = sqlite3.connect('receipts.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM receipts")
+    data = c.fetchall()
+    conn.close()
+    return render_template('query.html', data=data)
+
+if __name__ == '__main__':
+    create_table()
+    app.run(debug=True)
